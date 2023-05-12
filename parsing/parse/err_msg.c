289bd24cc9386190d03fb36326a54c98ad176567
Xerxes <amaarour.hicham97@gmail.com>
2023-05-12 13:16:55 +0100

    parsing

--- parsing/parse/err_msg.c
+++ parsing/parse/err_msg.c
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse.c                                            :+:      :+:    :+:   */
+/*   err_msg.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hamaarou <hamaarou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/10 12:56:45 by hamaarou          #+#    #+#             */
-/*   Updated: 2023/05/12 11:46:37 by hamaarou         ###   ########.fr       */
+/*   Created: 2023/05/12 12:35:15 by hamaarou          #+#    #+#             */
+/*   Updated: 2023/05/12 12:37:17 by hamaarou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/parsing.h"
 
-t_cmd	*start_parsing(t_parser *parser)
+
 {
-	//g_gob.ex_status = syntaxe_check(parser);
-	g_gob.ex_status = iterate_over_tokens_check_syntaxe(parser);
-	if (g_gob.ex_status != 258)
-	{
-		printf("correct\n");
-	}
-	// if suyntaxe error return NULL and free all the memory 
-	return (NULL);
+    ft_putendl_fd(msg, 2);
+    return (258);
 }
-
-
-
--- parsing/syntax/syntaxe_check.c
+++ parsing/syntax/syntaxe_check.c
@@ -6,7 +6,7 @@
 /*   By: hamaarou <hamaarou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 17:09:08 by hamaarou          #+#    #+#             */
-/*   Updated: 2023/05/11 19:25:39 by hamaarou         ###   ########.fr       */
+/*   Updated: 2023/05/12 12:38:24 by hamaarou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,51 @@ int	type_is_rederec(t_token *token)
 	return (1);
 }
 
+int	type_is_pipe(t_token *token)
+{
+	if (token)
+	{
+		if (token->type == t_PIPE)
+			return (1);
+	}
+	return (0);
+}
+int type_is_char(t_token *token)
+{
+	if (token)
+	{
+		if (token->type == t_CHAR)
+			return (1);
+	}
+	return (0);
+}
+
 int syntaxe_check(t_parser *parser)
 {
 	if(parser->current_token != NULL)
 	{
 		if(parser->current_token->type == t_PIPE || parser->current_token->type == t_EOF)
 		{
-			// expected is a T_CHAR
-			if (parser->current_token)
-			{
-				if (parser->current_token->type == t_CHAR)
-				{
-					
-				}
-				else
-				{
-					// syntaxe error
-					return (258);
-				}
-			}
+			if (type_is_char(parser->previous_token) == 0 || type_is_rederec(parser->previous_token) == 0)
+				return (err_msg("Bash : syntax error"));
+		}
+		else if (type_is_rederec(parser->current_token) == 0)
+		{
+			if (type_is_char(parser->previous_token) == 0)
+				return (err_msg("Bash : syntax error"));
 		}
 	}
+	return (0);
+}
+
+int iterate_over_tokens_check_syntaxe(t_parser *parser)
+{
+	while (parser->current_token->type != t_EOF)
+	{
+		if (syntaxe_check(parser) == 258)
+			return (258);
+		parser->previous_token = parser->current_token;
+		parser->current_token = get_next_token(parser->lexer);
+	}
+	return (0);
 }
