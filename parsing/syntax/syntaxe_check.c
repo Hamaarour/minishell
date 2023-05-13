0b4e9b41be862ad49858ffe67341d1d52ec27899
Xerxes <amaarour.hicham97@gmail.com>
2023-05-13 18:05:47 +0100

    syntaxe check is DONE

--- parsing/syntax/syntaxe_check.c
+++ parsing/syntax/syntaxe_check.c
@@ -6,7 +6,7 @@
 /*   By: hamaarou <hamaarou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 17:09:08 by hamaarou          #+#    #+#             */
-/*   Updated: 2023/05/12 18:59:35 by hamaarou         ###   ########.fr       */
+/*   Updated: 2023/05/13 18:03:53 by hamaarou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int type_is_char(t_token *token)
 	return (1);
 }
 
-t_tokens_type	type_is_rederec(t_token *token)
+int	type_is_rederec(t_token *token)
 {
 	if (token)
 	{
@@ -42,20 +42,31 @@ int	type_is_pipe(t_token *token)
 	}
 	return (1);
 }
+int type_hdoc_apnd(t_token *token)
+{
+	if (token)
+	{
+		if (token->type == t_HEREDOC || token->type == t_APPEND)
+			return (0);
+	}
+	return (1);
+}
 
 /* Check PIPE syntax */
-int	pipe_syntax(t_parser *parser)
+int	pipe_syntax(t_parser *parser, char *cmd)
 {
 	t_parser *tmp;
+	(void)parser;
 	
-	tmp = parser;
+	tmp = initialize_parser(cmd);
 	while (tmp->current_token->type != t_EOF)
 	{
 		if (tmp->current_token->type == t_PIPE)
 		{
+			
 			if (tmp->previous_token == NULL)
 				return (1);
-			else if (tmp->previous_token->type == t_PIPE)
+			else if (tmp->previous_token->type == t_PIPE || type_is_rederec(tmp->previous_token) == 0)
 				return (1);
 		}
 		tmp->previous_token = tmp->current_token;
@@ -63,31 +74,43 @@ int	pipe_syntax(t_parser *parser)
 		if (tmp->current_token->type == t_EOF && tmp->previous_token->type == t_PIPE)
 			return (1);	
 	}
+	free(tmp);
 	return (0);
 }
 
 /* Check redirection syntax */
-int	redirect_syntax(t_parser *parser)
+int	redirect_syntax(t_parser *parser, char *cmd)
 {
 	t_parser *tmp;
+	(void)parser;
 	
-	tmp = parser;
+	tmp = initialize_parser(cmd);
 	while (tmp->current_token->type != t_EOF)
 	{
-		if (type_is_rederec(tmp->current_token) != 10)
+		if (tmp->current_token->type == t_GREAT_THAN || tmp->current_token->type == t_LESS_THAN)
 		{
-			if (tmp->previous_token->type == type_is_rederec(tmp->current_token))
+			if (tmp->previous_token && (tmp->previous_token->type == t_GREAT_THAN || tmp->previous_token->type == t_LESS_THAN))
+				return (1);
+		}
+		else if (tmp->current_token->type == t_HEREDOC || tmp->current_token->type == t_APPEND)
+		{
+			if (!tmp->previous_token || tmp->previous_token->type == t_GREAT_THAN || tmp->previous_token->type == t_LESS_THAN )
 				return (1);
 		}
 		tmp->previous_token = tmp->current_token;
 		tmp->current_token = get_next_token(tmp->lexer);
+		if (tmp->current_token->type == t_EOF && type_is_rederec(tmp->previous_token) == 0)
+				return (1);	
 	}
+	free(tmp);
 	return (0);
 }
 
-int iterate_over_tokens_check_syntaxe(t_parser *parser)
+
+int iterate_over_tokens_check_syntaxe(t_parser *parser, char *cmd)
 {
-	if ((pipe_syntax(parser) == 1) || (redirect_syntax(parser) == 1))
+
+	if ((pipe_syntax(parser, cmd) == 1) || (redirect_syntax(parser, cmd) == 1))
 		return (err_msg("Bash : syntax error"));
 	
 	// printf("current_token : %u\n", parser->current_token->type);
