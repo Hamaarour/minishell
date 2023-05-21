2022f8e2417202d3b41ad97f975e894584c20ae7
Xerxes <amaarour.hicham97@gmail.com>
2023-05-21 20:38:28 +0100

    version befor mergin

--- parsing/parse/parse.c
+++ parsing/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: hamaarou <hamaarou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 12:56:45 by hamaarou          #+#    #+#             */
-/*   Updated: 2023/05/21 17:24:52 by hamaarou         ###   ########.fr       */
+/*   Updated: 2023/05/21 18:57:47 by hamaarou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,9 +95,11 @@ void	divid_cmd(t_parser *parser, t_data_cmd **cmd_data)
 
 void	start_parsing(t_parser *parser, t_data_cmd **cmd_data)
 {
+	(void)cmd_data;
 	g_gob.ex_status = iterate_over_tokens_check_syntaxe(parser);
 	if (g_gob.ex_status != 258)
 	{
 		divid_cmd(parser, cmd_data);
+		
 	}
 }
